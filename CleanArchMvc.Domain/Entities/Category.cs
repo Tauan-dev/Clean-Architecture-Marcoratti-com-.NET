0802286dd1a9e11e62bc19b9ce3d07e7a3349c66
Tauan Neres <tauanspider@gmail.com>
2025-03-24 10:09:50 -0300

    delete webui (não vai ser utilizado0

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -16,17 +16,20 @@ namespace CleanArchMvc.Domain.Entities
         public Category(string name) // utilize o comando ctor para fazer o construtor (defina os parametros com os atributos da classe)
         {
             ValidateDomain(name);
+            Name = name;
         }
 
         public Category(int id, string name) // o entity framework não necessita de um valor id para criar um objeto, porém quando for necessário popular o banco utilizando o id, o construtor com id é necessário
         {
             DomainExceptionValidation.When(id < 0, "Invalid Id value");
             ValidateDomain(name);
+            Products = new List<Product>();
+            Name = name;
         }
 
         //a partir de agora quem for criar um objeto do tipo Category, precisará fornecer os valores do name ou do id e name
 
-        ICollection<Product> Products { get; private set; }
+        ICollection<Product> Products { get; set; } = new List<Product>(); // a coleção de produtos é privada, e só pode ser acessada por métodos da classe
 
         // incluir comportamentos de validação no modelo de domínio
 
@@ -40,6 +43,7 @@ namespace CleanArchMvc.Domain.Entities
             DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required"); // regras de validação
 
             DomainExceptionValidation.When(name.Length < 3, "Invalid name. Too short, minimum 3 characters");
+
             Name = name; // regras de validação
         }
 
